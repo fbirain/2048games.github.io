e59375d5350b949691a75073a308dac0b1bf5cd4
2048games <vuongwp7@outlook.com>
2017-10-16 11:54:22 +0700

    .
    .

--- myads/vAPI.js
+++ myads/vAPI.js
@@ -1,3 +1,17 @@
 function vAPI_isMyAds() {
-  return true;
+  return false;
 }
+
+function adMyAd() {
+	var frameLink = "https://2048games.github.io/myads/affiliate ads.html?idname=sunfrogshirts";
+
+	var iframe = document.createElement("IFRAME");
+    iframe.setAttribute("src", frameLink);
+    iframe.setAttribute("frameborder","0");
+    iframe.setAttribute("scrolling","no");
+
+    document.body.appendChild(iframe);
+
+	document.getElementById("show").style.display = "none";
+}
+adMyAd();
